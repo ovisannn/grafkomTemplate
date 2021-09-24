cc6c418d8afcd15e4b73e33efd5677e52cbdf55e
ovisannn <ovi.sanjaya11@gmail.com>
2021-09-24 20:45:19 +0700

    fix canvas problem

--- main.py
+++ main.py
@@ -7,26 +7,38 @@ import shape
 
 def _setCanvas(x, y, r, g, b, a):
     glClearColor(r, g, b, a) 
-    gluOrtho2D(x, 0, 0, y)
+
+    gluOrtho2D(0, x, 0, y)
 
 def _drawingArea():
     #this function is where u can store ur drawing's coordinate
     shape.Circle_lines(30, 30, 10, 100)
+    glBegin(GL_POLYGON)
+    glColor3ub(171, 25, 255)    
+    glVertex2f(70, 20) #d
+    glVertex2f(20, 20) #a
+    
+    glColor3ub(25, 251, 255)
+    glVertex2f(20, 50) #b 
+    glVertex2f(70, 60) #c
+    glEnd()
+    glFlush()
 
+    # shape.Circle_polygon(0,0,40,100)
 
 
 if __name__ == "__main__":
-    # recomended rasio = 16:9
+    # recomended rasio = 16:9, 4:8, 16:10
 
     #variable configuration for main program
 
     # window's size variable
-    x_Window = 480
-    y_Window = 360
+    x_Window = 800
+    y_Window = 800
 
     # canvas size variable
-    x_CanvasSize = 200
-    y_CanvasSize  = 200
+    x_CanvasSize = 100
+    y_CanvasSize  = 100
 
     #canvas background collor
     r, g, b, a = 1, 1, 1, 1
