6552c6410669c925b7b566e2258f8b4fff2b30f2
ovisannn <ovi.sanjaya11@gmail.com>
2021-10-11 08:46:26 +0700

    add controller

--- main.py
+++ main.py
@@ -5,53 +5,75 @@ import sys
 from math import *
 import shape
 
-def _setCanvas(x, y, r, g, b, a):
-    glClearColor(r, g, b, a) 
+xPos = 20
+yPos = 20
 
+x = 0
+
+direction = 1
+# window's size variable
+x_Window = 800
+y_Window = 800
+
+# canvas size variable
+x_CanvasSize = 100
+y_CanvasSize  = 100
+
+#canvas background collor
+r, g, b, a = 1, 1, 1, 1
+def _setCanvas(x, y, r, g, b, a):
+    glClearColor(r, g, b, a)
     gluOrtho2D(0, x, 0, y)
 
 def _drawingArea():
-    #this function is where u can store ur drawing's coordinate
-    shape.Circle_lines(30, 30, 10, 100)
-    glBegin(GL_POLYGON)
-    glColor3ub(171, 25, 255)    
-    glVertex2f(70, 20) #d
-    glVertex2f(20, 20) #a
+    glClear(GL_COLOR_BUFFER_BIT)
+    global xPos, yPos, direction
     
-    glColor3ub(25, 251, 255)
-    glVertex2f(20, 50) #b 
-    glVertex2f(70, 60) #c
-    glEnd()
-    glFlush()
+    if xPos >= x_CanvasSize and yPos >= y_CanvasSize:
+        direction*=-1
 
-    # shape.Circle_polygon(0,0,40,100)
+    xPos += direction
+    yPos += direction
+    shape.Circle_polygon(xPos, yPos, 10, 100)
+
+
+def bola_gerak_vertikal(direction):
+    global yPos
+    yPos+=10*direction
+def bola_gerak_horizontal(direction):
+    global xPos
+    xPos+=10*direction
+
+def controller(key, x, y):
+    if key == GLUT_KEY_UP:
+        bola_gerak_vertikal(1)
+        print("atas")
+    if key == GLUT_KEY_DOWN:
+        bola_gerak_vertikal(-1)
+        print("bawah")
+    if key == GLUT_KEY_LEFT:
+        bola_gerak_horizontal(-1)
+        print("kiri")
+    if key == GLUT_KEY_RIGHT:
+        bola_gerak_horizontal(1)
+        print("kanan")
+    print(xPos,",", yPos)
+
+def update(value):
+    glutPostRedisplay()
+    glutTimerFunc(10,update,0)
 
 
 if __name__ == "__main__":
-    # recomended rasio = 16:9, 4:8, 16:10
-
-    #variable configuration for main program
-
-    # window's size variable
-    x_Window = 800
-    y_Window = 800
-
-    # canvas size variable
-    x_CanvasSize = 100
-    y_CanvasSize  = 100
-
-    #canvas background collor
-    r, g, b, a = 1, 1, 1, 1
-
-    # window's name variable
+  
     windowName = 'latihan grafkom'
-
-    # main function for running program
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB)
     glutInitWindowSize(x_Window, y_Window)
     glutInitWindowPosition(x_Window,y_Window)
     glutCreateWindow(windowName)
     glutDisplayFunc(_drawingArea)
+    glutSpecialFunc(controller)
     _setCanvas(x_CanvasSize, y_CanvasSize, r, g, b, a)
+    glutTimerFunc(50, update, 0)
     glutMainLoop()
--- shape.py
+++ shape.py
@@ -4,14 +4,16 @@ from OpenGL.GL import*
 from OpenGL.GLU import*
 from OpenGL.GLUT import*
 
-def Circle_polygon(x_pos, y_pos, radius, sides):          
-    glBegin(GL_POLYGON)    
+def Circle_polygon(x_pos, y_pos, radius, sides): 
+    glBegin(GL_POLYGON)   
+    glColor3ub(3, 211, 252) 
     for i in range(100):    
         cosine= radius * cos(i*2*pi/sides) + x_pos  
         sine  = radius * sin(i*2*pi/sides) + y_pos   
         glVertex2f(cosine,sine)
     glEnd()
     glFlush()
+
 
 def Circle_lines(x_pos, y_pos, radius, sides):
     glBegin(GL_LINE_STRIP)    
